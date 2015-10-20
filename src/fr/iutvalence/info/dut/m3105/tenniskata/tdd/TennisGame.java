522ec75c2e805480adf7fc4599bd31b19c7f7df7
Adrian <adrian.amaglio@iut-valence.fr>
2015-10-20 17:10:41 +0200

    Added the receiver scoring test. Removed a line feed.

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -2,8 +2,9 @@ package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 public class TennisGame
 {
 
-	private static final String FIFTEEN_LOVE = "fifteen-love";
-	private static final String LOVE_ALL = "love-all";
+	public static final String LOVE_FIFTEEN = "love-fifteen";
+	public static final String FIFTEEN_LOVE = "fifteen-love";
+	public static final String LOVE_ALL = "love-all";
 	
 	private String score;
 	
@@ -19,6 +20,11 @@ public class TennisGame
 
 	public void serverScores() {
 		this.score = FIFTEEN_LOVE;
+	}
+
+	public void receiverScores() {
+		this.score = LOVE_FIFTEEN;
+		
 	}
 
 
--- test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
+++ test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
@@ -2,21 +2,34 @@ package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TennisGameTest
 {
+	
+	
+	private TennisGame theTennisGameUnderTestRightNow;
+
+	@Before
+	public void newTennisGame(){
+		this.theTennisGameUnderTestRightNow = new TennisGame();
+	}
+	
 	@Test
 	public void weAreMakingSureThatAGameThatJustBegunReturnAScoreEqualsToTheSusdescribedStringLoveAll(){
-		TennisGame theTennisGameUnderTestRightNow = new TennisGame();
-		Assert.assertEquals(theTennisGameUnderTestRightNow.getScore(), "love-all");
-		
+		Assert.assertEquals(this.theTennisGameUnderTestRightNow.getScore(), TennisGame.LOVE_ALL);
 	}
 	
 	@Test
 	public void weChangeTheScoreIfThePlayerThatServedWonAPointReturnEqualsToTheSusdescribedStringFifteenLove(){
-		TennisGame theTennisGameUnderTestRightNow = new TennisGame();
-		theTennisGameUnderTestRightNow.serverScores();
-		Assert.assertEquals(theTennisGameUnderTestRightNow.getScore(), "fifteen-love");
+		this.theTennisGameUnderTestRightNow.serverScores();
+		Assert.assertEquals(this.theTennisGameUnderTestRightNow.getScore(), TennisGame.FIFTEEN_LOVE);
+	}
+	
+	@Test
+	public void weChangeTheScoreIfThePlayerThatReceivedWonAPointReturnEqualsToTheSusdescribedStringFifteenLove(){
+		this.theTennisGameUnderTestRightNow.receiverScores();
+		Assert.assertEquals(this.theTennisGameUnderTestRightNow.getScore(), TennisGame.FIFTEEN_LOVE);
 	}
 }
